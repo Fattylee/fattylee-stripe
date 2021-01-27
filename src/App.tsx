c07130b8103485fe6f9a72bd202bef00dee04afd
Fatai Balogun <fattylee.remod@gmail.com>
2021-01-27 13:15:58 +0100

    legacy code, test me query

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { gql } from "apollo-boost";
+import { Mutation, Query } from "react-apollo";
 
-function App() {
+const GET_ME = gql`
+  query ME {
+    me {
+      id
+      email
+    }
+  }
+`;
+
+const LOGIN = gql`
+  mutation LOGIN($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      id
+      email
+    }
+  }
+`;
+
+export const App = () => {
+  const [login, setLogin] = useState({ email: "", password: "" });
+
+  const handleCHange = (e: any) => {
+    // console.log(e.target.name);
+    // const [name, value] = e.target;
+    console.log(e.target.name);
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(name, value);
+    setLogin((state) => ({ ...state, [name]: value }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Mutation mutation={LOGIN}>
+      {(mutate: any) => (
+        <Query query={GET_ME}>
+          {({ data, loading, error }: any) => {
+            if (loading) return <p>Loading...</p>;
 
-export default App;
+            if (error) return <p>Error: {error.message}</p>;
+
+            if (!data) return <p>No data</p>;
+
+            return (
+              <div>
+                {JSON.stringify(data, null, 1)}
+                <h1>this is good</h1>
+                <p>this is a paragraph</p>
+
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    mutate({
+                      update(_: any, data: any) {
+                        console.log(data);
+                      },
+                      variables: {
+                        email: login.email,
+                        password: login.password,
+                      },
+                    });
+                  }}
+                >
+                  <input
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    value={login.email}
+                    onChange={handleCHange}
+                  />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={login.password}
+                    onChange={handleCHange}
+                  />
+                  <input type="submit" value="Login" />
+                </form>
+              </div>
+            );
+          }}
+        </Query>
+      )}
+    </Mutation>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import React from "react";
+import "./index.css";
+import { App } from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
+// Pass your GraphQL endpoint to uri
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  credentials: "include",
+});
+
+const ApolloApp = (
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>
 );
+
+render(ApolloApp, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
